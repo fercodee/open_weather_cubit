0174a66944f56057c58cde1fe2c4cfbbb1fcb4ef
fercodee <devbyfull@gmail.com>
2024-02-28 00:05:41 -0300

    kickoff project

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:open_weather_cubit/cubits/weathers/weather_cubit.dart';
+import 'package:open_weather_cubit/pages/home_page.dart';
+import 'package:open_weather_cubit/repositories/weather_repository.dart';
+import 'package:open_weather_cubit/services/weather_api_services.dart';
+import 'package:http/http.dart' as http;
+
+void main() async {
+  await dotenv.load(fileName: '.env');
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider(
+      create: (context) => WeatherRepository(
+          weatherApiServices: WeatherApiServices(
+        httpClient: http.Client(),
+      )),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<WeatherCubit>(
+            create: (context) => WeatherCubit(
+              weatherRepository: context.read<WeatherRepository>(),
+            ),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Weather App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const HomePage(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/repositories/weather_repository.dart
@@ -0,0 +1,33 @@
+import 'package:open_weather_cubit/exceptions/weather_exception.dart';
+import 'package:open_weather_cubit/models/custom_error.dart';
+import 'package:open_weather_cubit/models/direct_geocoding.dart';
+import 'package:open_weather_cubit/models/weather.dart';
+import 'package:open_weather_cubit/services/weather_api_services.dart';
+
+class WeatherRepository {
+  final WeatherApiServices weatherApiServices;
+  WeatherRepository({
+    required this.weatherApiServices,
+  });
+
+  Future<Weather> fetchWeather(String city) async {
+    try {
+      final DirectGeocoding directGeocoding =
+          await weatherApiServices.getDirectGeocoding(city);
+
+      final Weather tempWeather =
+          await weatherApiServices.getWeather(directGeocoding);
+
+      final Weather weather = tempWeather.copyWith(
+        name: directGeocoding.name,
+        country: directGeocoding.country,
+      );
+
+      return weather;
+    } on WeatherException catch (e) {
+      throw CustomError(errMsg: e.message);
+    } catch (e) {
+      throw CustomError(errMsg: e.toString());
+    }
+  }
+}
